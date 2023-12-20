bdde166ad2c6011ffa8237405f6eb4186da791f9
Vanessa Choque Surco <91792207+VanSC@users.noreply.github.com>
2023-12-20 09:54:43 -0500

    Add files via upload

---
+++ airbnb-api/src/mock/places.ts
@@ -0,0 +1,31 @@
+export const places = {
+  places: [
+    {
+      name: "Estadio Inca Garcilaso de la vega",
+      rating: 4.5,
+      location: { country: "Peru", city: "Cusco" },
+      image_url: "https://www.copesco.gob.pe/wp-content/uploads/2018/12/47376635_1384525565023733_4249109646712766464_o.jpg",
+      price: 120.99,
+      longitud: -13.524666731595108,
+      latitud: -71.96652765829296,
+    },
+    {
+      name: "Adventure National Park Lodge",
+      rating: 4.2,
+      location: { country: "Canada", city: "Vancouver" },
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/f/fb/T%C3%BApac_Amaru_II.JPG",
+      price: 95.75,
+      longitud: -13.522064087269616,
+      latitud: -71.96645647682648,
+    },
+    {
+      name: "Seafront Restaurant Retreat",
+      rating: 4.8,
+      location: { country: "United Kingdom", city: "London" },
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Plaza_de_Cusco_Allison_Bellido.jpg/640px-Plaza_de_Cusco_Allison_Bellido.jpg",
+      price: 225.5,
+      longitud: -13.515140077568416,
+      latitud: -71.97883616207407,
+    }
+  ],
+};
